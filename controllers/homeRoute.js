80b423593bbf4b2c3c1b3dd9e5fbec12556eb271
Dobby <apple@Dobbyde-MacBook-Pro.local>
2023-01-27 08:55:35 +0800

    edit homeroute

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -4,12 +4,7 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
-        const TagData = await Tag.findAll({
-            include: [
-                // { model: User, attributes: ['user_name'] },
-                // { model: Tag },
-            ],
-        });
+        const TagData = await Tag.findAll();
         const tags = TagData.map((tag) => tag.get({ plain: true }));
         res.render('homepage', {
             tags,
@@ -29,6 +24,39 @@ router.get('/addRecipe', withAuth, async (req, res) => {
         const user = userData.get({ plain: true });
 
         res.render('addRecipe', {
+            ...user,
+            logged_in: true,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/tag/:id', async (req, res) => {
+    try {
+        const TagData = await Tag.findByPk(req.params.id, {
+            include: [{ model: Recipe }],
+        });
+        const tag = TagData.get({ plain: true });
+        res.render('tag', {
+            ...tag,
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Recipe }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
             ...user,
             logged_in: true,
         });
