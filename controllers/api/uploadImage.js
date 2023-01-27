95f42fa5ab458795e81043461d78e871d1fdfe6f
bambolelooo <63548697+bambolelooo@users.noreply.github.com>
2023-01-26 23:31:32 -0500

    cleared the code a bit

--- controllers/api/uploadImage.js
+++ controllers/api/uploadImage.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const router = require('express').Router();
 const cloudinary = require('cloudinary').v2;
-
+const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 const storage = new CloudinaryStorage({
@@ -10,21 +10,20 @@ const storage = new CloudinaryStorage({
         folder: 'DEV',
     },
 });
-
-const multer = require('multer');
 const upload = multer({ storage: storage });
-// var cl = new cloudinary.Cloudinary({cloud_name: "dhniskpvl", secure: true});
 
 cloudinary.config({
     cloud_name: process.env.cloud_name,
     api_key: process.env.api_key,
     api_secret: process.env.api_secret,
 });
-
 console.log(cloudinary.config().cloud_name);
 
 router.post('/', upload.single('picture'), async (req, res) => {
-    if (!req.file) return res.send('Please upload a file');
+    if (!req.file)
+        return res.send(
+            'Please upload a file, if using Insomnia select MultiPart form and with field name of "picture"'
+        );
     return res.json({ picture: req.file.path });
 });
 module.exports = router;
