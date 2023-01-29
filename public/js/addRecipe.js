e5c2dfca38e1ee39d37c6f2705b5efef6ff70b55
bambolelooo <63548697+bambolelooo@users.noreply.github.com>
2023-01-29 18:42:53 -0500

    rebase

--- public/js/addRecipe.js
+++ public/js/addRecipe.js
@@ -96,3 +96,61 @@ const newFrom = async (event) => {
         }
     }
 };
+/* eslint-disable no-unused-vars */
+$(document).ready(function () {
+    function previewImage(event) {
+        /**
+         * Get the selected files.
+         */
+        const imageFiles = event.target.files;
+        /**
+         * Count the number of files selected.
+         */
+        const imageFilesLength = imageFiles.length;
+        /**
+         * If at least one image is selected, then proceed to display the preview.
+         */
+        if (imageFilesLength > 0) {
+            /**
+             * Get the image path.
+             */
+            const imageSrc = URL.createObjectURL(imageFiles[0]);
+            /**
+             * Select the image preview element.
+             */
+            const imagePreviewElement = document.querySelector(
+                '#preview-selected-image'
+            );
+            /**
+             * Assign the path to the image preview element.
+             */
+            imagePreviewElement.src = imageSrc;
+            /**
+             * Show the element by changing the display value to "block".
+             */
+            imagePreviewElement.style.display = 'block';
+        }
+    }
+
+    $('#add-ingredient').click((event) => {
+        event.preventDefault();
+        const id =
+            Number(
+                $('#ingredients input:last').attr('id').replace('quantity_', '')
+            ) + 1;
+        console.log(id);
+        $('#ingredients').append(`<input
+    type="text"
+    name="ingredient_${id}"
+    id="ingredient_${id}"
+    placeholder="Egg"
+    class="yellow text-black rounded-xl text-lg p-2 font-thin w-full bg-white"
+/> <input
+    type="text"
+    name="quantity_${id}"
+    id="quantity_${id}"
+    placeholder="2 pcs"
+    class="yellow text-black w-full rounded-xl text-lg p-2 font-thin bg-white"
+/>`);
+    });
+});
