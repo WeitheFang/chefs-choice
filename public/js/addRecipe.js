9a284b6c001e4845b362d1a692e757443822a856
Dobby <apple@Dobbyde-MacBook-Pro.local>
2023-01-30 21:28:20 +0800

    add js file in public

--- public/js/addRecipe.js
+++ public/js/addRecipe.js
@@ -77,3 +77,22 @@ $(document).ready(function () {
 />`);
     });
 });
+const newFrom = async (event) => {
+    event.preventDefault();
+    const title = document.querySelector('#title').value.trim();
+    const ingredients = document.querySelector('#ingredients').value.trim();
+    const instructions = document.querySelector('#instructions').value.trim();
+    if (title && ingredients && instructions) {
+        const response = await fetch('/api/recipes', {
+            method: 'POST',
+            body: JSON.stringify({ title, ingredients, instructions }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (response.ok) {
+            // If successful, redirect the browser to the main page
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
