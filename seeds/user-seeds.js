c5cdb3488ee1f89f9da1c27af42072dfe7832fca
Dobby <apple@Dobbyde-MacBook-Pro.local>
2023-01-31 09:14:05 +0800

    fix both login handlebars and .js and bcrypt password in seed

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -4,13 +4,15 @@ const userData = [
     {
         user_name: 'maria',
         email: 'chefmaria@gmail.com',
-        password: 'Ilovefood1234!',
+        password:
+            '$2a$10$rcBlxD8d.4RpdO5HqYiuPujJ7GD2nYovWqIvojXCTYiXtaUvXJTEC', //Ilovefood1234!,
         is_Admin: true,
     },
     {
         user_name: 'jackthefoody',
         email: 'pineapplesonpizza@gmail.com',
-        password: '@Burgers2424',
+        password:
+            '$2a$10$/3fIchxxDSxuq5FJuO5VO.yFJomZYk8/9DJIvGgUit2P.0bz/.IIO', //@Burgers2424
         is_Admin: false,
     },
 ];
