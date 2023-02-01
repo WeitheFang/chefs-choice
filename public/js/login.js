a0f12d33b0687582931b83f9545c8fb612b838fc
Dobby <apple@Dobbyde-MacBook-Pro.local>
2023-02-01 10:06:52 +0800

    fix sigin up request

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,6 @@
 const loginForm = async (event) => {
     event.preventDefault();
+    console.log(`click`);
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
     if (email && password) {
@@ -19,13 +20,13 @@ const loginForm = async (event) => {
 
 const signupForm = async (event) => {
     event.preventDefault();
-    const username = document.querySelector('#username-signup').value.trim();
+    const user_name = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-    if (name && email && password) {
+    if (user_name && email && password) {
         const response = await fetch('/api/users', {
             method: 'POST',
-            body: JSON.stringify({ username, email, password }),
+            body: JSON.stringify({ user_name, email, password }),
             headers: { 'Content-Type': 'application/json' },
         });
         if (response.ok) {
