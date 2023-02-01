c87e545ee96ec42e68ef43d57317a2634f5b9256
bambolelooo <63548697+bambolelooo@users.noreply.github.com>
2023-02-01 00:02:49 -0500

    fixed image preview

--- public/js/addRecipe.js
+++ public/js/addRecipe.js
@@ -68,40 +68,6 @@ const newFrom = async (event) => {
 
 document.querySelector('.new-recipe-form').addEventListener('submit', newFrom); //change the query selector to match the form class name
 $(document).ready(function () {
-    function previewImage(event) {
-        /**
-         * Get the selected files.
-         */
-        const imageFiles = event.target.files;
-        /**
-         * Count the number of files selected.
-         */
-        const imageFilesLength = imageFiles.length;
-        /**
-         * If at least one image is selected, then proceed to display the preview.
-         */
-        if (imageFilesLength > 0) {
-            /**
-             * Get the image path.
-             */
-            const imageSrc = URL.createObjectURL(imageFiles[0]);
-            /**
-             * Select the image preview element.
-             */
-            const imagePreviewElement = document.querySelector(
-                '#preview-selected-image'
-            );
-            /**
-             * Assign the path to the image preview element.
-             */
-            imagePreviewElement.src = imageSrc;
-            /**
-             * Show the element by changing the display value to "block".
-             */
-            imagePreviewElement.style.display = 'block';
-        }
-    }
-
     $('#add-ingredient').click((event) => {
         event.preventDefault();
         const id =
